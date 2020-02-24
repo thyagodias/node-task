72274169b9101fd4a11afc79f8b4ef8e484b401b
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-02-24 11:02:33 -0400

    adiciona arquivo de configurações do db

---
+++ libs/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  database: 'nodetask',
+  username: '',
+  password: '',
+  params: {
+    dialect: 'sqlite',
+    storage: 'nodetask.sqlite',
+    define: {
+      underscored: true
+    }
+  }
+}
