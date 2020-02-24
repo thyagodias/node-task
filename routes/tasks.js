a77ae32de36ec0f53aafc0e99287d8f09c92c9e7
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-02-24 17:59:29 -0400

    configura respostas de recursos das rotas

--- routes/tasks.js
+++ routes/tasks.js
@@ -13,20 +13,32 @@ module.exports = app => {
           res.status(412).json({ msg: error.message })
         })
     })
-    .post((req, res) => {
-      Tasks.create(req.body)
-        .then(result => res.json(result))
+  app
+    .route('/tasks/:id')
+    .get((req, res) => {
+      Tasks.findOne({ where: req.params })
+        .then(result => {
+          if (result) {
+            res.json(result)
+          } else {
+            res.sendStatus(404)
+          }
+        })
         .catch(error => {
           res.status(412).json({ msg: error.message })
         })
     })
-  app
-    .route('/tasks/:id')
-    .all((req, res, next) => {
-      delete req.body.id
-      next()
+    .put((req, res) => {
+      Tasks.update(req.body, { where: req.params })
+        .then(result => res.sendStatus(204))
+        .catch(error => res.status(412).json({ msg: error.msg }))
+        .finally(() => {})
     })
-    .get((req, res) => {})
-    .put((req, res) => {})
-    .delete((req, res) => {})
+    .delete((req, res) => {
+      Tasks.destroy({ where: req.params })
+        .then(result => res.sendStatus(204))
+        .catch(error => {
+          res.status(412).json({ msg: error })
+        })
+    })
 }
