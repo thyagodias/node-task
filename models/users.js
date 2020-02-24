940bf24d1c9f33d3040067f0edbb4a2268f6e356
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-02-24 11:43:49 -0400

    cria modelo de usuários

---
+++ models/users.js
@@ -0,0 +1,41 @@
+module.exports = (sequelize, DataType) => {
+  const Users = sequelize.define(
+    'Users',
+    {
+      id: {
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      name: {
+        type: DataType.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      password: {
+        type: DataType.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      },
+      email: {
+        type: DataType.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        }
+      }
+    },
+    {
+      classMethods: {
+        associate: models => {
+          Users.hasMany(models.Task)
+        }
+      }
+    }
+  )
+}
