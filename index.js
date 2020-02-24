65693728b6c65e3cbe19d5320f036b1a0c3631fc
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-02-24 11:09:21 -0400

    inclui o modulo de conexão no app

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import consign from 'consign'
 const app = express()
 
 consign()
-  .include('models')
+  .include('db.js')
+  .then('models')
   .then('libs/middlewares.js')
   .then('routes')
   .then('libs/boot.js')
