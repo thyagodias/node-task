3e5591ddff8f2fb6e0d1a49c0f96b64c771022bc
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-02-24 16:18:08 -0400

    adiciona funções do endpoint /tasks

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,8 +1,32 @@
 module.exports = app => {
   const Tasks = app.db.models.Tasks
-  app.get('/tasks', (req, res) => {
-    Tasks.findAll({}).then(tasks => {
-      res.json({ tasks: tasks })
+  app
+    .route('/tasks')
+    .all((req, res, next) => {
+      delete req.body.id
+      next()
     })
-  })
+    .get((req, res) => {
+      Tasks.findAll({})
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({ msg: error.message })
+        })
+    })
+    .post((req, res) => {
+      Tasks.create(req.body)
+        .then(result => res.json(result))
+        .catch(error => {
+          res.status(412).json({ msg: error.message })
+        })
+    })
+  app
+    .route('/tasks/:id')
+    .all((req, res, next) => {
+      delete req.body.id
+      next()
+    })
+    .get((req, res) => {})
+    .put((req, res) => {})
+    .delete((req, res) => {})
 }
