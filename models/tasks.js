6474a989f01bde441e51c411c957b58318091309
Thyago Dias <thyagojuniorrochadias@gmail.com>
2020-02-24 11:43:37 -0400

    cria modelo de tarefas

--- models/tasks.js
+++ models/tasks.js
@@ -1,7 +1,32 @@
-module.exports = app => {
-  return {
-    findAll: (params, callback) => {
-      return callback([{ title: 'Fazer	compras' }, { title: 'Consertar	o	pc' }])
+module.exports = (sequelize, DataType) => {
+  const Tasks = sequelize.define(
+    'Tasks',
+    {
+      id: {
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: DataType.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true
+        },
+        done: {
+          type: DataType.BOOLEAN,
+          allowNull: false,
+          defaultValue: false
+        }
+      }
+    },
+    {
+      classMethods: {
+        associate: models => {
+          Task.belongsTo(models.Users)
+        }
+      }
     }
-  }
+  )
+  return Tasks
 }
